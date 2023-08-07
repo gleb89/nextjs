ace1258110397b15633f12d8142dcfb283b7e731
gleb89 <glebhleb89@icloud.com>
2023-08-07 12:03:20 +0300

    auth

---
+++ src/pages/auth/index.js
@@ -0,0 +1,11 @@
+export default function AuthPage(params) {
+    async function submitContact(event) {
+        event.preventDefault();
+        console.log(4556);
+    }
+  return (
+    <>
+      <h1>auth</h1>
+    </>
+  );
+}
